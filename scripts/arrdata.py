c46c38f676608c223d62743059c5bf2d55c8e512
Michiel De Wilde <michiel.dewilde@keysight.com>
2021-01-01 18:49:44 +0100

    vmix

--- scripts/arrdata.py
+++ scripts/arrdata.py
@@ -12,3 +12,7 @@ def add_arrdata(arrdata):
 add_arrdata(Arrdata('2', 5, 24, 'kbox'))
 add_arrdata(Arrdata('3', 13, 32, 'kbox'))
 add_arrdata(Arrdata('11', 23, 190, 'mbox'))
+add_arrdata(Arrdata('a', 136, 153, 'kbox'))
+add_arrdata(Arrdata('b', 142, 160, 'kbox'))
+add_arrdata(Arrdata('c', 149, 167, 'kbox'))
+add_arrdata(Arrdata('d', 156, 173, 'kbox'))
