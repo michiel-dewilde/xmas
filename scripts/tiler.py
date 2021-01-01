3ceb40007e056d25b2188ada42ab73c1d92e1bd8
Michiel De Wilde <michiel.dewilde@keysight.com>
2021-01-01 18:31:29 +0100

    cw, ccw

--- scripts/tiler.py
+++ scripts/tiler.py
@@ -1,20 +1,6 @@
 import json, math
 import numpy as np
 
-def calc_w(layout, weights):
-    if isinstance(layout, str):
-        return weights[layout]
-    assert isinstance(layout, dict)
-    assert len(layout) == 1
-    key, value = next(iter(layout.items()))
-    if key == 'bg':
-        return float(value)
-    if key == 'h' or key == 'v':
-        return sum(calc_w(item, weights) for item in value)
-    if 'key == cw' or key == 'ccw':
-        return sum((calc_w(item, weights) for item in value['d']), calc_w(value['c'], weights))
-    assert False
-
 def set_seq(first, second):
     first.next = second
     second.prev = first
@@ -75,7 +61,22 @@ class Ie: # initial edge
 class Tiling:
     def round(self, val):
         return np.round(val)
-    def create_tile(self, layout, tishe, lishe, bishe, rishe):
+
+    def calc_w(self,layout):
+        if isinstance(layout, str):
+            return self.weights[layout]
+        assert isinstance(layout, dict)
+        assert len(layout) == 1
+        key, value = next(iter(layout.items()))
+        if key == 'bg':
+            return float(value)
+        if key == 'h' or key == 'v':
+            return sum(self.calc_w(item) for item in value)
+        if 'key == cw' or key == 'ccw':
+            return sum((self.calc_w(item) for item in value['d']), self.calc_w(value['c']))
+        assert False
+
+    def process_layout(self, layout, tishe, lishe, bishe, rishe):
         if isinstance(layout, str):
             tile = Tile(layout, tishe, lishe, bishe, rishe)
             self.tiles.append(tile)
@@ -90,7 +91,7 @@ class Tiling:
         left, top = tishe.pmin
         right, bot = bishe.pmax
         if key == 'h':
-            weights = [calc_w(item, self.weights) for item in value]
+            weights = [self.calc_w(item) for item in value]
             cweights = np.cumsum(weights)
             xs = self.round(left + cweights[:-1]*((right - left) / cweights[-1]))
             ptops = [(x, top) for x in xs]
@@ -102,10 +103,10 @@ class Tiling:
             nlishes = [lishe] + [ie.hes[1].first_ishe for ie in ies]
             nrishes = [ie.hes[0].first_ishe for ie in ies] + [rishe]
             for nlayout, ntishe, nlishe, nbishe, nrishe in zip(value, ntishes, nlishes, nbishes, nrishes):
-                self.create_tile(nlayout,  ntishe, nlishe, nbishe, nrishe)
+                self.process_layout(nlayout,  ntishe, nlishe, nbishe, nrishe)
             return
         if key == 'v':
-            weights = [calc_w(item, self.weights) for item in value]
+            weights = [self.calc_w(item) for item in value]
             cweights = np.cumsum(weights)
             ys = self.round(top + cweights[:-1]*((bot - top) / cweights[-1]))
             plefts = [(left, y) for y in ys]
@@ -117,13 +118,13 @@ class Tiling:
             ntishes = [tishe] + [ie.hes[0].first_ishe for ie in ies]
             nbishes = [ie.hes[1].first_ishe for ie in ies] + [bishe]
             for nlayout, ntishe, nlishe, nbishe, nrishe in zip(value, ntishes, nlishes, nbishes, nrishes):
-                self.create_tile(nlayout,  ntishe, nlishe, nbishe, nrishe)
+                self.process_layout(nlayout,  ntishe, nlishe, nbishe, nrishe)
             return
         if 'key == cw' or key == 'ccw':
             W = right - left
             H = bot - top
-            g = calc_w(value['c'], self.weights)
-            G = sum((calc_w(item, self.weights) for item in value['d']), g)
+            g = self.calc_w(value['c'])
+            G = sum((self.calc_w(item) for item in value['d']), g)
             a = value['a']
             w = math.sqrt(a*W*H*g/G)
             h = w/a
@@ -135,20 +136,66 @@ class Tiling:
             y1 = self.round(top + (H-h)/2)
             y2 = self.round(top + (H+h)/2)
             y3 = bot
-            #maaktegels(tegels, value['c'], vopts, {'x':x1, 'y':y1, 'w':x2-x1, 'h':y2-y1})
-            #if ('key' == 'cw'):
-            #    maaktegels(tegels, value['d'][0], vopts, {'x':x0, 'y':y0, 'w':x2-x0, 'h':y1-y0})
-            #    maaktegels(tegels, value['d'][1], vopts, {'x':x2, 'y':y0, 'w':x3-x2, 'h':y2-y0})
-            #    maaktegels(tegels, value['d'][2], vopts, {'x':x1, 'y':y2, 'w':x3-x1, 'h':y3-y2})
-            #    maaktegels(tegels, value['d'][3], vopts, {'x':x0, 'y':y1, 'w':x1-x0, 'h':y3-y1})
-            #else:
-            #    maaktegels(tegels, value['d'][0], vopts, {'x':x0, 'y':y0, 'w':x1-x0, 'h':y2-y0})
-            #    maaktegels(tegels, value['d'][1], vopts, {'x':x0, 'y':y2, 'w':x2-x0, 'h':y3-y2})
-            #    maaktegels(tegels, value['d'][2], vopts, {'x':x2, 'y':y1, 'w':x3-x2, 'h':y3-y1})
-            #    maaktegels(tegels, value['d'][3], vopts, {'x':x1, 'y':y0, 'w':x3-x1, 'h':y1-y0})
+            if ('key' == 'cw'):
+                toishes = tishe.split([(x2, y0)])
+                roishes = rishe.split([(x3, y2)])
+                boishes = bishe.split([(x1, y3)])
+                loishes = lishe.split([(x0, y1)])
+
+                tiie = Ie('h', (x0, y1), (x2, y1))
+                biishe = tiie.hes[1]
+                tcishes = tiie.hes[0].split([x1, y1])                
+
+                riie = Ie('v', (x2, y0), (x2, y2))
+                liishe = riie.hes[1]
+                rcishes = riie.hes[0].split([x2, y1])                
+
+                biie = Ie('h', (x1, y2), (x3, y2))
+                tiishe = biie.hes[0]
+                bcishes = biie.hes[1].split([x2, y2])                
+                
+                liie = Ie('v', (x1, y1), (x3, y1))
+                riishe = liie.hes[0]
+                lcishes = liie.hes[1].split([x2, y1])                
+
+                self.ies += [tiie, riie, biie, liie]
+                self.process_layout(value['c'],  tcishes[1], lcishes[0], bcishes[0], rcishes[1])
+                self.process_layout(value['d'][0],  toishes[0], loishes[0], biishe, rcishes[0])
+                self.process_layout(value['d'][1],  toishes[1], liishe, bcishes[1], roishes[0])
+                self.process_layout(value['d'][2],  tiishe, lcishes[1], boishes[1], roishes[1])
+                self.process_layout(value['d'][3],  tcishes[0], loishes[1], boishes[0], riishe)
+            else:
+                toishes = tishe.split([(x1, y0)])
+                loishes = lishe.split([(x0, y2)])
+                boishes = bishe.split([(x2, y3)])
+                roishes = rishe.split([(x3, y1)])
+
+                liie = Ie('v', (x0, y1), (x2, y1))
+                riishe = liie.hes[0]
+                lcishes = liie.hes[1].split([x1, y1])
+
+                biie = Ie('h', (x0, y2), (x2, y2))
+                tiishe = biie.hes[0]
+                bcishes = biie.hes[1].split([x1, y2])                
+                
+                riie = Ie('v', (x2, y1), (x2, y3))
+                liishe = riie.hes[1]
+                rcishes = riie.hes[0].split([x2, y2])                
+
+                tiie = Ie('h', (x1, y1), (x3, y1))
+                biishe = tiie.hes[1]
+                tcishes = tiie.hes[0].split([x2, y1])                
+
+                self.ies += [liie, biie, riie, tiie]
+                self.process_layout(value['c'],  tcishes[0], lcishes[1], bcishes[1], rcishes[0])
+                self.process_layout(value['d'][0],  toishes[0], loishes[0], bcishes[0], riishe)
+                self.process_layout(value['d'][1],  tiishe, loishes[1], boishes[0], rcishes[1])
+                self.process_layout(value['d'][2],  tcishes[1], liishe, boishes[1], roishes[1])
+                self.process_layout(value['d'][3],  toishes[1], lcishes[0], biishe, roishes[0])
             return
         assert False
-    def calc_tiling(self):
+
+    def calc_tiling(self, layout):
         topleft = (0, 0)
         topright = (self.size[0], 0)
         botleft = (0, self.size[1])
@@ -158,14 +205,14 @@ class Tiling:
         self.ibe = Ie('h', botleft, botright)
         self.ire = Ie('v', topright, botright)
         self.ies += [self.ite, self.ile, self.ibe, self.ire]
-        self.create_tile(self.layout, self.ite.hes[0].first_ishe, self.ile.hes[1].first_ishe, self.ibe.hes[1].first_ishe, self.ire.hes[0].first_ishe)
+        self.process_layout(layout, self.ite.hes[0].first_ishe, self.ile.hes[1].first_ishe, self.ibe.hes[1].first_ishe, self.ire.hes[0].first_ishe)
+        
     def __init__(self, size, layout, weights):
         self.size = size
-        self.layout = layout
         self.weights = weights
         self.ies = []
         self.tiles = []
-        self.calc_tiling()
+        self.calc_tiling(layout)
 
 tiling = Tiling(size = (1920,1080), layout = json.loads('{"h":[{"v":["a","a","a"]},{"v":["a","a"]}]}'), weights = {'a': 1.0})
 
