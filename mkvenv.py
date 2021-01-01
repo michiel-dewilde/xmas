379b52b53d37bc8376e768732f183ce8d218d552
Michiel De Wilde <michiel.dewilde@keysight.com>
2021-01-01 18:30:36 +0100

    mkvenv

---
+++ mkvenv.py
@@ -0,0 +1,7 @@
+import subprocess, os, sys
+if not os.path.exists('venv'):
+    subprocess.call([sys.executable, '-m', 'venv', 'venv'])
+vpy = os.path.join('venv', 'Scripts', 'python.exe')
+subprocess.call([vpy, '-m', 'pip', 'install', '--upgrade', 'pip', 'setuptools', 'wheel'])
+vpip = os.path.join('venv', 'Scripts', 'pip.exe')
+subprocess.call([vpip, 'install', '--upgrade', 'aggdraw', 'pillow', 'numpy==1.19.3', 'scipy', 'moviepy'])
